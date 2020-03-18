afccf67704c08f1fa5e6d0cd1ecea1042855e57a
its_vrd <varad.desai.rl@gmail.com>
2020-03-18 12:30:17 -0400

    for testing deployment 1

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,5 +1,9 @@
+# copy the new bundle to desired location
+cd `ls -td -- /opt/codedeploy-agent/deployment-root/4801f4db-c413-4cbf-a0ac-fb4276d3588e/* | head -n 1`
+cp bundle.tar /home/ubuntu
+
 # untar the zip
-tar -xzvf webapp.tar.gz
+tar -xzvf bundle.tar
 
 # move to project root location
 cd project
--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,5 +1,12 @@
 # close application and clear jar from the location
 
-# clear build_creation location
 
-# bring the tar to build_creation location
+# clear build_creation location
+cd /home/ubuntu
+pwd
+rm bundle.tar
+rm -rf project
+rm ROOT*.jar
+
+# kill tomcat process
+pkill -9 -f tomcat
