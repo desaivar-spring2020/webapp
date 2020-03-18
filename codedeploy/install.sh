95f836097ff5c3a6ea2e3fe1a62179b360cb7c7c
its_vrd <varad.desai.rl@gmail.com>
2020-03-18 16:41:55 -0400

    for testing build creation 17

--- codedeploy/install.sh
+++ codedeploy/install.sh
@@ -3,15 +3,4 @@
 #cd `ls -td -- /opt/codedeploy-agent/deployment-root/4801f4db-c413-4cbf-a0ac-fb4276d3588e/* | head -n 1`
 #cp bundle.tar /home/ubuntu
 
-# untar the zip
-cd /home/ubuntu
-tar -xzvf webapp.tar.gz
 
-# move to project root location
-cd /home/ubuntu/project
-
-# generate build
-mvn clean install
-
-# copy build file to location from which it should run
-cp /home/ubuntu/project/target/ROOT*.jar /home/ubuntu
