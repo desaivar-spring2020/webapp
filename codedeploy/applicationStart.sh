a0dc27591c3bb2d0cd452f687927725a58629023
its_vrd <varad.desai.rl@gmail.com>
2020-03-18 20:23:25 -0400

    for testing build creation 2

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -9,7 +9,7 @@ cd /home/ubuntu/project
 # generate build
 mvn clean install
 
-# copy build file to location from which it should run
+# copy build file to location from which it should run.
 cp /home/ubuntu/project/target/ROOT*.jar /home/ubuntu
 sudo pkill -9 -f tomcat
 cd /home/ubuntu
