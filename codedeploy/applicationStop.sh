7a8305822ed6f24c44d1e92343498831fe94c288
its_vrd <varad.desai.rl@gmail.com>
2020-03-25 02:33:10 -0400

    deployment testing

--- codedeploy/applicationStop.sh
+++ codedeploy/applicationStop.sh
@@ -4,11 +4,11 @@ echo "***************************************************"
 echo "APPLICATION STOP BEGINS"
 echo "***************************************************"
 
-cd /home/ubuntu
-sudo kill -9 $(lsof -t -i:8080) nohup java -jar /home/ubuntu/ROOT*.jar server.port=8080 > /home/ubuntu/application-execution.out 2>&1 &
-sudo pkill -9 -f tomcat
-
-sudo chmod 777 /home/ubuntu/codedeploy/beforeInstall.sh
+#cd /home/ubuntu
+#sudo kill -9 $(lsof -t -i:8080) nohup java -jar /home/ubuntu/ROOT*.jar server.port=8080 > /home/ubuntu/application-execution.out 2>&1 &
+#sudo pkill -9 -f tomcat
+#
+#sudo chmod 777 /home/ubuntu/codedeploy/beforeInstall.sh
 
 echo "***************************************************"
 echo "APPLICATION STOP ENDS"
