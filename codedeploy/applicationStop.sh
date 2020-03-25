b26399c6cc1ff54018337f6ca2fde3ea39a596a0
its_vrd <varad.desai.rl@gmail.com>
2020-03-25 02:52:41 -0400

    deployment testing

--- codedeploy/applicationStop.sh
+++ codedeploy/applicationStop.sh
@@ -4,7 +4,8 @@ echo "***************************************************"
 echo "APPLICATION STOP BEGINS"
 echo "***************************************************"
 
-#cd /home/ubuntu
+cd /home/ubuntu
+mkdir applicationStop
 #sudo kill -9 $(lsof -t -i:8080) nohup java -jar /home/ubuntu/ROOT*.jar server.port=8080 > /home/ubuntu/application-execution.out 2>&1 &
 #sudo pkill -9 -f tomcat
 #
