d9f000e87aec96251b87b43cf1197f14b350bf26
its_vrd <varad.desai.rl@gmail.com>
2020-04-02 20:21:03 -0400

    errors in deployment

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,28 +1,28 @@
-echo "***************************************************"
-echo "BEFORE INSTALL BEGINS"
-echo "***************************************************"
-# move to home location
-cd /home/ubuntu
-pwd
-ls -ltrh
-
-
-# clear home location
-cd /home/ubuntu
-pwd
-sudo rm webapp.tar.gz
-sudo rm -rf project
-sudo rm ROOT*.jar
-sudo rm appspec.yml
-sudo rm -rf codedeploy
-
-# kill tomcat process (redundant command, just to be safe)
-sudo pkill -9 -f tomcat
-
-# make next file executable
-sudo chmod 777 /home/ubuntu/codedeploy/install.sh
-
-
-echo "***************************************************"
-echo "BEFORE INSTALL ENDS"
-echo "***************************************************"
+#echo "***************************************************"
+#echo "BEFORE INSTALL BEGINS"
+#echo "***************************************************"
+## move to home location
+#cd /home/ubuntu
+#pwd
+#ls -ltrh
+#
+#
+## clear home location
+#cd /home/ubuntu
+#pwd
+#sudo rm webapp.tar.gz
+#sudo rm -rf project
+#sudo rm ROOT*.jar
+#sudo rm appspec.yml
+#sudo rm -rf codedeploy
+#
+## kill tomcat process (redundant command, just to be safe)
+#sudo pkill -9 -f tomcat
+#
+## make next file executable
+##sudo chmod 777 /home/ubuntu/codedeploy/install.sh
+#
+#
+#echo "***************************************************"
+#echo "BEFORE INSTALL ENDS"
+#echo "***************************************************"
--- codedeploy/install.sh
+++ codedeploy/install.sh
@@ -1,17 +1,17 @@
-
-# copy the new bundle to desired location
-#cd `ls -td -- /opt/codedeploy-agent/deployment-root/4801f4db-c413-4cbf-a0ac-fb4276d3588e/* | head -n 1`
-#cp bundle.tar /home/ubuntu
-
-
-echo "***************************************************"
-echo "INSTALL BEGINS"
-echo "***************************************************"
-
-# make next file executable
-sudo chmod 777 /home/ubuntu/codedeploy/afterInstall.sh
-
-
-echo "***************************************************"
-echo "INSTALL ENDS."
-echo "***************************************************"
+#
+## copy the new bundle to desired location
+##cd `ls -td -- /opt/codedeploy-agent/deployment-root/4801f4db-c413-4cbf-a0ac-fb4276d3588e/* | head -n 1`
+##cp bundle.tar /home/ubuntu
+#
+#
+#echo "***************************************************"
+#echo "INSTALL BEGINS"
+#echo "***************************************************"
+#
+## make next file executable
+#sudo chmod 777 /home/ubuntu/codedeploy/afterInstall.sh
+#
+#
+#echo "***************************************************"
+#echo "INSTALL ENDS."
+#echo "***************************************************"
