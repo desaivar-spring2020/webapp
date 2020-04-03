71ecc62e9c1f6cffdaa46be1541c3354d482584b
its_vrd <varad.desai.rl@gmail.com>
2020-04-02 21:32:22 -0400

    for deployment

--- codedeploy/downloadBundle.sh
+++ codedeploy/downloadBundle.sh
@@ -4,6 +4,11 @@
 #
 #echo "just skip this"
 #
+
+cd /home/ubuntu
+rm -rf applicationStop
+rm -rf applicationStart
+
 #sudo chmod 777 /home/ubuntu/codedeploy/applicationStop.sh
 #
 #echo "***************************************************"
