a89910d108be2f3bc5928a8f32eccd902fd79597
its_vrd <varad.desai.rl@gmail.com>
2020-04-11 00:23:57 -0400

    removing dependency of s3_bucket_user super final 2

--- src/main/java/com/csye/user/service/AmazonClient.java
+++ src/main/java/com/csye/user/service/AmazonClient.java
@@ -7,6 +7,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
